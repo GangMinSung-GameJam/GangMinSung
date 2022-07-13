2f264dbb1a5605a6ddcfea23893f985bf17fe869
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2022-07-13 16:42:26 +0900

    웨이브 텍스처
    웨이브 텍스처

--- Assets/scripts/UiManager.cs
+++ Assets/scripts/UiManager.cs
@@ -6,9 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour
 {
+    float time;
+    float fadeRate = 0;
     [SerializeField] GameObject penal;
-
+    [SerializeField] Text wavePenal;
     
+
+    private void Update()
+    {
+        Fade();
+        
+    }
     public void Startbutton(string name)
     {
         SceneManager.LoadScene(name);
@@ -25,5 +33,19 @@ public class UiManager : MonoBehaviour
     {
         SceneManager.LoadScene(start);
     }
-
+    void Fade()
+    {
+        if (time <= 1)
+        {
+            wavePenal.color = new Color(1, 1, 1, fadeRate);
+            fadeRate += 0.007f;
+        }
+        if (time >= 2)
+        {
+            wavePenal.color = new Color(1, 1, 1, fadeRate);
+            fadeRate -= 0.007f; 
+        }
+        
+    }
+    
 }
