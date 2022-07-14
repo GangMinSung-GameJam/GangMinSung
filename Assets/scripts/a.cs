785cae57167e65c3925a078088390c9aef5ce0bc
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2022-07-14 15:35:17 +0900

    증강체
    증강체

--- Assets/scripts/a.cs
+++ Assets/scripts/a.cs
@@ -4,10 +4,52 @@ using UnityEngine;
 
 public class a : MonoBehaviour
 {
-   
+    public int selectedBullet;
+    public bool stop = false;
+    [SerializeField] Upgrade upgrade;
     void Update()
     {
+        GameObject.Find("Player").SetActive(false);
         Time.timeScale = 0;
         GameObject.Find("EnemySpwaner").GetComponent<EnemySpawn>().stop = true;
+        SelectBullet();
+        swap();
+
+    }
+    public void SelectBullet()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == selectedBullet) { weapon.gameObject.SetActive(true); }
+            else { weapon.gameObject.SetActive(false); }
+            i++;
+        }
+    }
+    void swap()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (selectedBullet >= 2)
+                selectedBullet = 0;
+            else
+                selectedBullet++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if(GameObject.Find("playerHp") != null)
+            {
+                upgrade.PlayerHpUp();
+            }
+            if (GameObject.Find("loadTimeDown") != null)
+            {
+                upgrade.loadTime();
+            }
+            if (GameObject.Find("damageUp") != null)
+            {
+                upgrade.damageUp();
+            }
+        }
     }
 }
